bf934071552fc669246a5158c9b7f97ebb2034a6
Abhinn Ankit <ankit.a@husky.neu.edu>
2019-01-27 16:10:49 -0500

    Add webapp project skeleton

---
+++ webapp/src/main/java/com/csye6225/noteapp/NoteAppRestController.java
@@ -0,0 +1,9 @@
+package com.csye6225.noteapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class NoteAppRestController {
+}
