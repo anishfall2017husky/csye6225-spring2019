3d4b888e4eee985ff917e098d2bad373701272df
Abhinn Ankit <ankit.a@husky.neu.edu>
2019-01-27 21:01:21 -0500

    Add basic user authentication

--- webapp/src/main/java/com/csye6225/noteapp/NoteAppRestController.java
+++ webapp/src/main/java/com/csye6225/noteapp/NoteAppRestController.java
@@ -1,6 +1,8 @@
 package com.csye6225.noteapp;
 
 import com.csye6225.noteapp.models.User;
+import com.csye6225.noteapp.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,14 +14,18 @@ import java.util.Date;
 @RequestMapping("/")
 public class NoteAppRestController {
 
+    @Autowired
+    private UsersService usersService;
+
     @RequestMapping(value="/", method= RequestMethod.GET, produces = "application/json")
     public Date greeting() {
         return new Date();
     }
 
     @RequestMapping(value = "/user/register", method = RequestMethod.POST, produces = "application/json")
-    public User createNewDatabase(@RequestParam String username, @RequestParam String password) {
-        return new User(username, password);
+    public User register(@RequestParam String username, @RequestParam String password) {
+        User newUser = new User(username, password);
+        return usersService.registerUser(newUser);
     }
 
 }
