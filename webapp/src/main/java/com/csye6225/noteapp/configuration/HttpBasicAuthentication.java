03c9691537068d6f765cc2f6d2dd43fa9c0b5231
Abhinn Ankit <ankit.a@husky.neu.edu>
2019-01-29 18:44:42 -0500

    Post call returns JSON

--- webapp/src/main/java/com/csye6225/noteapp/configuration/HttpBasicAuthentication.java
+++ webapp/src/main/java/com/csye6225/noteapp/configuration/HttpBasicAuthentication.java
@@ -1,10 +1,11 @@
 package com.csye6225.noteapp.configuration;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.AuthenticationEntryPoint;
+
 
 @Configuration
 @EnableWebSecurity
--- webapp/src/main/java/com/csye6225/noteapp/models/User.java
+++ webapp/src/main/java/com/csye6225/noteapp/models/User.java
@@ -1,18 +1,28 @@
 package com.csye6225.noteapp.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
+@Table(name = "auth_db")
 public class User {
 
     @Id
+    @Column(name = "user_id")
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
 
+    @Column(name = "email")
+    @Email(message = "*Please provide a valid Email")
+    @NotEmpty(message = "*Please provide an email")
     private String emailAddress;
+
+    @Column(name = "password")
+    @Length(min = 8, message = "*Your password must have at least 8 characters")
+    @NotEmpty(message = "*Please provide your password")
     private String password;
 
     public User() {
