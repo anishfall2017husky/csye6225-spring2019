d9ef085c393741d8d71165eec7fb9462180dfffb
Anish Surti <surti.a@husky.neu.edu>
2019-01-30 08:32:13 -0800

    modified HttpBasicAuth class

--- webapp/src/main/java/com/csye6225/noteapp/configuration/HttpBasicAuthentication.java
+++ webapp/src/main/java/com/csye6225/noteapp/configuration/HttpBasicAuthentication.java
@@ -1,10 +1,12 @@
 package com.csye6225.noteapp.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.AuthenticationEntryPoint;
+
 
 @Configuration
 @EnableWebSecurity
@@ -23,6 +25,12 @@ public class HttpBasicAuthentication extends WebSecurityConfigurerAdapter {
                 .authenticationEntryPoint(authEntryPoint)
                 .and()
                 .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("{noop}secret1").roles("USER");
     }
 
 }
