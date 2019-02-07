fafb7dba643cba50658a80149750454fe7601fa7
Anish Surti <surti.a@husky.neu.edu>
2019-02-07 15:06:46 -0500

    added Fn:Sub in cf template

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -18,12 +18,12 @@ if aws cloudformation describe-stacks --stack-name $STACK_NAME &> /dev/null; the
 fi
 
 ##Creating Stack
-aws cloudformation create-stack --stack-name "$STACK_NAME" --template-body file://csye6225-cf-networking.json --parameters file://parameters.json
+aws cloudformation create-stack --stack-name "$STACK_NAME" --template-body file://csye6225-cf-networking.json --parameters ParameterKey=StackName,ParameterValue=$STACK_NAME
 echo "Creation in progress.."
 
 aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
 
-if [ $? -ne 0 ]; 
+if [ $? -ne 0 ];
 then
 	echo "Stack $STACK_NAME creation failed!"
     exit 1
