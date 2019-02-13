63ae51ed5b45c2b0404972918fa2354e1d2a38b2
Srikant Swamy <swamy.sr@husky.neu.edu>
2019-02-13 11:37:35 -0800

    Modified awscli teardown

--- infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
@@ -2,45 +2,20 @@
 #!/bin/bash
 
 # ************* Script to create AWS resources using awscli *********************************
+# Input Parameters:
 #
-#     vpc_id               --   VPC ID     
-#     subnetid1 	       --   Subnet ID-1
-#     subnetid2            --   Subnet ID-2
-#     subnetid3            --   Subnet ID-3
-#     publicRouteTableId   --   Public Route Table ID
-#     internetGatewayId    --   Internet Gateway ID
+#  			     tag_name   --   Tag Name    
 #
 # Change Log:
 #
-# Feb 5 2019 -- Nilank Sharma  -- Initial Creation
-# Feb 6 2019 -- Srikant Swamy  -- Modified
+# Feb 05 2019 -- Nilank Sharma  -- Initial Creation
+# Feb 06 2019 -- Srikant Swamy  -- Modified
+# Feb 12 2019 -- Srikant Swamy  -- Modified inputs to delete resources based on tag name
 #
 # ********************************************************************************************
 
-echo "Please enter SUBNET-1 ID:"
-read subnetId1
-
-echo "Please enter SUBNET-2 ID:"
-read subnetId2
-
-echo "Please enter SUBNET-3 ID:"
-read subnetId3
-
-echo "Please enter INTERNET GATEWAY ID:"
-read internetGatewayId
-
-echo "Please enter PUBLIC ROUTE TABLE ID:"
-read publicRouteTableId
-
-echo "Please enter VPC ID:"
-read vpcid
-
-#vpcid=$(echo $VpcId)
-#subnetId1=$1
-#subnetId2=$2
-#subnetId3=$3
-#publicRouteTableId=$4
-#internetGatewayId=$5
+echo "Please enter tag name of vpc to delete:"
+read tag_name
 
 echo -e "\n"
 echo " ************************************************** "
@@ -48,164 +23,261 @@ echo " ********** Networking teardown started *********** "
 echo " ************************************************** "
 echo -e "\n"
 
+echo " ********* Fetching VPC ID *********** "
 
+step="Fetch: VPC ID"
+
+vpc_id=$(aws ec2 describe-vpcs \
+--filters Name=tag:Name,Values=$tag_name \
+--query 'Vpcs[0].VpcId' --output text)
+
+flag=$?
+
+if [ -z  "$vpc_id" ] || [ "$vpc_id" = "None" ]
+then
+
+flag=1
+
+fi
+
+if [ $flag -ne 0 ]
+then
+	
+	echo -e "\n"
+	echo " **************************************************** "
+	echo " **** Exiting: Failed at - $step with exit status: $flag  **** "
+	echo " **************************************************** "
+	echo " ************ Teardown Process Complete ************* "
+	echo -e "\n"
+	exit 1
+	
+fi
+
+echo " ******* VPC ID Lookup Complete ******* "
+
+echo " ******** Fetching Subnet Ids *********** "
+
+step="Fetch: Subnet Id for the VPC TAG"
+
+subnet_ids=$(aws ec2 describe-subnets \
+--filters Name=vpc-id,Values=$vpc_id \
+--query 'Subnets[].SubnetId' --output text)
+
+flag=$?
+
+if [ -z  "$subnet_ids" ] || [ "$subnet_ids" = "None" ]
+then
+
+flag=1
+
+fi
+
+if [ $flag -ne 0 ]
+then
+	echo -e "\n"
+	echo " **************************************************** "
+	echo " **** Exiting: Failed at - $step with exit status: $flag *** "
+	echo " **************************************************** "
+	echo " ************ Teardown Process Complete ************* "
+	echo -e "\n"
+	exit 1
+fi
+
+echo " ******* Fetch Subnet Complete ******* "
 
 # Deleting Subnets
 
 echo " ****** Deleting Subnets ******* "
 
-echo " ****** Deleting Subnet-1 ****** "
+step="Delete: Subnet"
 
-step="Deletion: Subnet 1"
+for subnet_id in ${subnet_ids}; do
+	aws ec2 delete-subnet --subnet-id $subnet_id
 
-aws ec2 delete-subnet --subnet-id $subnetId1
+	flag=$?
 
-flag=$?
+	if [ $flag -ne 0 ]; then
+	
+		echo -e "\n"
+		echo " **************************************************** "
+		echo " **** Exiting: Failed at - $step with exit status: $flag **** "
+		echo " **************************************************** "
+		echo " ************ Teardown Process Complete ************* "
+		echo -e "\n"
+		exit 1
+	else
+		echo " ******* SUBNET: ${subnet_id} deleted ******** "
+	fi
+done
 
-if [ $flag -ne 0 ]; then
-	flag=1
-	echo -e "\n"
-	echo " **************************************************** "
-	echo "Exiting: Failed at step: $step with exit status: $flag"
-	echo " ************ Teardown Process Complete ************* "
-	echo -e "\n"
-	exit 1
-else
-	echo " ******* Deleted Subnet $subnetId1 ******* "
-fi
 
-step="Deletion: Subnet 2"
+# Deleting public route table
 
-echo " ****** Deleting Subnet-2 ****** "
-aws ec2 delete-subnet --subnet-id $subnetId2
+step="Fetch: Public Route Table"
 
-flag=$?
+echo " **** Fetching Public Route Table **** "
 
-if [ $flag -ne 0 ]; then
-	flag=1
-	echo -e "\n"
-	echo " **************************************************** "
-	echo "Exiting: Failed at step: $step with exit status: $flag"
-	echo " ************ Teardown Process Complete ************* "
-	echo -e "\n"
-	exit 1
-else
-	echo " ******* Deleted Subnet $subnetId2 ******* "
-fi
-
-step="Deletion: Subnet 3"
-
-echo " ****** Deleting Subnet-3 ****** "
-aws ec2 delete-subnet --subnet-id $subnetId3
+route_table_ids=$(aws ec2 describe-route-tables \
+--filters Name=vpc-id,Values=$vpc_id Name=tag:Name,Values=$tag_name \
+--query 'RouteTables[].RouteTableId' --output text)
 
 flag=$?
 
-if [ $flag -ne 0 ]; then
-	flag=1
+if [ -z  "$route_table_ids" ] || [ "$route_table_ids" = "None" ]
+then
+
+flag=1
+
+fi
+
+if [ $flag -ne 0 ]
+then
+	
 	echo -e "\n"
 	echo " **************************************************** "
-	echo "Exiting: Failed at step: $step with exit status: $flag"
+	echo " **** Exiting: Failed at - $step with exit status: $flag **** "
+	echo " **************************************************** "
 	echo " ************ Teardown Process Complete ************* "
 	echo -e "\n"
 	exit 1
-else
-	echo " ******* Deleted Subnet $subnetId3 ******* "
 fi
 
+echo " ******* Fetch Route Table Complete ******* "
 
-echo " ****** Deleted Subnets ******** "
+step="Delete: Public Route Table"
 
-#Deleting public route table
+echo " **** Deleting Route Table ************* "
 
-step="Deletion: Public Route Table"
+for route_table_id in ${route_table_ids}; do
+	aws ec2 delete-route-table --route-table-id $route_table_id
 
-echo " **** Deleting Public Route Table **** "
+	flag=$?
 
-aws ec2 delete-route-table --route-table-id $publicRouteTableId
+	if [ $flag -ne 0 ]; then
+	
+		echo -e "\n"
+		echo " **************************************************** "
+		echo " **** Exiting: Failed at - $step with exit status: $flag **** "
+		echo " **************************************************** "
+		echo " ************ Teardown Process Complete ************* "
+		echo -e "\n"
+		exit 1
+	else
+		echo " ******* Route Table ID: ${route_table_id} deleted ******** "
+	fi
+done
 
 flag=$?
 
-if [ $flag -ne 0 ]; then
-	flag=1
-	echo -e "\n"
-	echo " **************************************************** "
-	echo "Exiting: Failed at step: $step with exit status: $flag"
-	echo " ************ Teardown Process Complete ************* "
-	echo -e "\n"
-	exit 1
-else
-	echo " ******* Deleted public route table $publicRouteTableId ******* "
-fi
 
 # Detach Internet Gateway
+
+step="Fetch: Internet Gateway"
+
+echo " ******** Fetching Internet Gateway ******** "
+
+igw_ids=$(aws ec2 describe-internet-gateways \
+--filter Name=attachment.vpc-id,Values=$vpc_id \
+--query InternetGateways[].InternetGatewayId --output text)
+
+flag=$?
+
+if [ -z  "$igw_ids" ] || [ "$igw_ids" = "None" ]
+then
+
+flag=1
+
+fi
+
+if [ $flag -ne 0 ]
+then
+	
+	echo -e "\n"
+	echo " **************************************************** "
+	echo " **** Exiting: Failed at - $step with exit status: $flag **** "
+	echo " **************************************************** "
+	echo " ************ Teardown Process Complete ************* "
+	echo -e "\n"
+	exit 1
+fi
 
 step="Detach: Internet Gateway"
 
 echo " ******** Detaching Internet Gateway ******** "
 
-aws ec2 detach-internet-gateway --internet-gateway-id $internetGatewayId --vpc-id $vpcid
+for igw_id in ${igw_ids}; do
+	aws ec2 detach-internet-gateway --internet-gateway-id $igw_id --vpc-id $vpc_id
+	
+	flag=$?
 
-flag=$?
+	if [ $flag -ne 0 ]; then
+	
+		echo -e "\n"
+		echo " **************************************************** "
+		echo " **** Exiting: Failed at - $step with exit status: $flag **** "
+		echo " **************************************************** "
+		echo " ************ Teardown Process Complete ************* "
+		echo -e "\n"
+		exit 1
+	else
+		echo " ******* Internet Gateway ID: ${igw_id} detached ******** "
+	fi
+done
 
-if [ $flag -ne 0 ]; then
-	flag=1
-	echo -e "\n"
-	echo " **************************************************** "
-	echo "Exiting: Failed at step: $step with exit status: $flag"
-	echo " ************ Teardown Process Complete ************* "
-	echo -e "\n"
-	exit 1
-else
-	echo " ****** Detached internet gateway $internetGatewayId ****** "
-fi
 
 # Deleting Internet gateway
 
-step="Deletion: Internet Gateway"
+step="Delete: Internet Gateway"
 
 echo " ****** Deleting Internet Gateway ****** "
 
-aws ec2 delete-internet-gateway --internet-gateway-id $internetGatewayId
+for igw_id in ${igw_ids}; do
+	aws ec2 delete-internet-gateway --internet-gateway-id $igw_id
 
-flag=$?
+	flag=$?
+	
+	if [ $flag -ne 0 ]; then
+	
+		echo -e "\n"
+		echo " **************************************************** "
+		echo " **** Exiting: Failed at - $step with exit status: $flag **** "
+		echo " **************************************************** "
+		echo " ************ Teardown Process Complete ************* "
+		echo -e "\n"
+		exit 1
+	else
+		echo " ******* Internet Gateway ID: ${igw_id} deleted ******** "
+	fi	
+done
 
-if [ $flag -ne 0]; then
-	flag=1
-	echo -e "\n"
-	echo " **************************************************** "
-	echo "Exiting: Failed at step: $step with exit status: $flag"
-	echo " ************ Teardown Process Complete ************* "
-	echo -e "\n"
-	exit 1
-else
-	echo " ****** Deleted Internet Gateway $internetGatewayId ****** "
-fi
 
-# Deleteing VPC
+# Deleting VPC
 
-step="Deletion: VPC"
+step="Delete: VPC"
 
 echo " ******* Deleting VPC ******** "
 
-aws ec2 delete-vpc --vpc-id $vpcid
+aws ec2 delete-vpc --vpc-id $vpc_id
 
 flag=$?
 
-if [ $flag -ne 0 ]; then
-	flag=1
+if [ $flag -ne 0 ]
+then
+	
 	echo -e "\n"
 	echo " **************************************************** "
-	echo "Exiting: Failed at step: $step with exit status: $flag"
+	echo " **** Exiting: Failed at - $step with exit status: $flag **** "
+	echo " **************************************************** "
 	echo " ************ Teardown Process Complete ************* "
 	echo -e "\n"
 	exit 1
 else 
-	echo " ***** VPC $vpcid Deleted ******* "
+	echo " ***** VPC: $vpc_id Deleted ******* "
 fi
 
 echo -e "\n"
 
-echo " ************** All resources deleted ************** "
+echo " ************** All Resources Deleted ************** "
 echo " *************************************************** "
 echo " ********** Networking teardown completed ********** "
 echo " *************************************************** "
