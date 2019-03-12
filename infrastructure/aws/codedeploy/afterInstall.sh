bac992456e0d835c7f178b1d06fecb362d276fe1
Nilank Sharma <sharmanilank@gmail.com>
2019-03-12 15:26:54 -0400

    Add start stop script for application

---
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo systemctl daemon-reload
+sudo systemctl stop tomcat.service
+
+sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT
+
+sudo mv /opt/tomcat/webapps/*.war /opt/tomcat/webapps/ROOT.war
+
+sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
+
+# cleanup log files
+sudo rm -rf /opt/tomcat/logs/catalina*
+sudo rm -rf /opt/tomcat/logs/*.log
+sudo rm -rf /opt/tomcat/logs/*.txt
